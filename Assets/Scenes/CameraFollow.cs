7abb1ba26e710e21d89a922de3b82978b8333d90
sihoyang1-max <sihoyang1@gmail.com>
2026-04-14 12:04:33 +0900

    [Feat] 캐릭터 점프 개선, 글라이더 추가, 카메라 따라가기 추가

---
+++ Assets/Scenes/CameraFollow.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset = new Vector3(0, 5, -10);
+    public float smoothSpeed = 0.125f;
+
+    private void LateUpdate()
+    {
+        
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
